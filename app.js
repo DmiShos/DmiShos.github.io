c463d4dce55a6c359744c05476f6b50b74070433
DmiShos <waltz.no2.shostakovich@gmail.com>
2024-03-04 00:21:19 +0200

    Added main buttons and market list

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
-let tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;
 
-tg.MainButton.show();
+const testCompany1 = {
+    companyName: "Durger King",
+    companyDecsription: "Fun Fun Fun!!!",
+};
+const testCompany2 = {
+    companyName: "Bulochka",
+    companyDecsription: "Wanna eat",
+};
+const testCompany3 = {
+    companyName: "Mac Fried",
+    companyDecsription: "Let's cook some shit!",
+};
+const companies = [testCompany1, testCompany2, testCompany3];
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData("1");
+const backButton = document.getElementById("back");
+const mainButtons = document.getElementById("mainbuttons");
+const companyList = document.getElementById("companylist");
+hide(companyList);
+
+let currentScreen = "main"
+
+backButton.addEventListener("click", function() {
+    switch (currentScreen) {
+        case "main":
+            window.close()
+            break;
+
+        case "market":
+            currentScreen = "main"
+            hide(companyList)
+            showMainButtons()
+            break;
+    
+        default:
+            break;
+    }
 });
+
+mainButtons.children.item(0).addEventListener("click", function() {
+    currentScreen = "market"
+
+    hideMainButtons()
+    for (let index = 0; index < companyList.children.length; index++) {
+        const element = companyList.children.item(index);
+        const currentCompany = companies[index];
+        element.children.item(0).innerHTML = currentCompany.companyName;
+        element.children.item(1).innerHTML = currentCompany.companyDecsription;
+    };
+    show(companyList);
+});
+
+function showMainButtons() {
+    for (let index = 0; index < mainButtons.children.length; index++) {
+        const element = mainButtons.children.item(index);
+        show(element);
+    };
+};
+
+function hideMainButtons() {
+    for (let index = 0; index < mainButtons.children.length; index++) {
+        const element = mainButtons.children.item(index);
+        hide(element);
+    };
+};
+
+function hide(element) {
+    element.style.display = "none";
+};
+
+function show(element) {
+    element.style.display = "block";
+};
